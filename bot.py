1bb074bbee501fb7ae28168f5d45709057e810f8
Artem Siliuk <asiliuk@mindk.com>
2016-10-27 13:11:17 +0300

    5. Answer exchange. First inline keyboard callback handler experience.

--- bot.py
+++ bot.py
@@ -36,5 +36,30 @@ def exchange_command(message):
     )
 
 
+# handle all callback queries with data starts with 'ex-'
+@bot.callback_query_handler(func=lambda query: query.data.startswith('ex-'))
+def choose_exchange_callback(query):
+    # get chosen currency from callback query (ex-<currency>)
+    chosen_currency = query.data[3:]
+
+    chat_id = query.message.chat.id
+
+    # disables 'loading' state and adds 'typing' title to chat
+    bot.answer_callback_query(query.id)
+    bot.send_chat_action(chat_id, 'typing')
+
+    # get data from pb_service by chosen currency
+    currency_result = pb_service.get_exchange_by_currency(chosen_currency)
+
+    base_currency = currency_result['base_ccy']
+    bot.send_message(
+        chat_id=chat_id,
+        text='<i>Currency exchange for ' + currency_result['ccy'] + ':</i>\n\n' +
+             '<b>Buy:</b> ' + currency_result['buy'] + ' ' + base_currency + '\n' +
+             '<b>Sale:</b> ' + currency_result['sale'] + ' ' + base_currency,
+        parse_mode='HTML'
+    )
+
+
 # use long poll non-stop
 bot.polling(none_stop=True)

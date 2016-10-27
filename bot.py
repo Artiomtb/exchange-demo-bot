81bf77d42df6695bd5e21b1143a467ce9a5d65cd
Artem Siliuk <asiliuk@mindk.com>
2016-10-27 11:31:40 +0300

    1. /start command handler

--- bot.py
+++ bot.py
@@ -4,5 +4,15 @@ from telebot import TeleBot
 # init bot by token from config
 bot = TeleBot(config.TOKEN)
 
+
+# handle /start
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(
+        chat_id=message.chat.id,
+        text='Welcome! I\' m glad to see you!'
+    )
+
+
 # use long poll non-stop
 bot.polling(none_stop=True)
